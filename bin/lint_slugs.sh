45b67f03fdb7011a162756fdf45e9f02d3e90bca
David Brownman <beamneocube@gmail.com>
2024-01-18 13:42:19 -0800

    check slug uniqueness separately in stories and posts

--- bin/lint_slugs.sh
+++ bin/lint_slugs.sh
@@ -1,6 +1,11 @@
-duplicate_slugs=$(jq -r '.. | .slug?' config.json | sort | uniq -d)
-if [ -n "$duplicate_slugs" ]; then
-    echo "\033[0;31mThe following slugs are not unique:\033[0m"
-    echo "$duplicate_slugs"
-    exit 1
-fi
+function check_slugs() {
+    duplicate_slugs=$(jq -r ".[\"$1\"][].slug" config.json | sort | uniq -d)
+    if [ -n "$duplicate_slugs" ]; then
+        echo "\033[0;31mThe following $1 slugs are not unique:\033[0m"
+        echo "$duplicate_slugs"
+        exit 1
+    fi
+}
+
+check_slugs posts
+check_slugs stories

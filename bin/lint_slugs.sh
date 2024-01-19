eefb35a92bf8f3ad3b81c613f60e1c4b5de50ae7
David Brownman <beamneocube@gmail.com>
2024-01-19 00:10:57 -0800

    tweak arg passing to bash function

--- bin/lint_slugs.sh
+++ bin/lint_slugs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function check_slugs() {
-    duplicate_slugs=$(jq -r ".[\"$1\"][].slug" config.json | sort | uniq -d)
+    duplicate_slugs=$(jq -r --arg key $1 '.[$key][].slug' config.json | sort | uniq -d)
     if [ -n "$duplicate_slugs" ]; then
-        echo "\033[0;31mThe following $1 slugs are not unique:\033[0m"
+        echo -e "\033[0;31mThe following $1 slugs are not unique:\033[0m"
         echo "$duplicate_slugs"
         exit 1
     fi
